09cf1e0f12b4168acc749ffd39b45acb07ccce95
RafaelGondi <rdias.ga@gmail.com>
2022-11-12 00:00:14 -0300

    feat: Passa a exportar ícones como javascript e typescript

--- svgo.config.js
+++ svgo.config.js
@@ -1,15 +1,21 @@
 module.exports = {
 	multipass: true,
 	js2svg: {
-		indent: 2,
-		pretty: true,
+	  indent: 2,
+	  pretty: true,
 	},
 	plugins: [
-		{ name: 'preset-default' },
-		'sortAttrs',
-		'removeScriptElement',
-		'removeDimensions',
-		'removeScriptElement',
-		'removeDimensions',
+	  { name: 'preset-default' },
+	  'sortAttrs',
+	  'removeScriptElement',
+	  'removeDimensions',
+	  'removeScriptElement',
+	  'removeDimensions',
+	  {
+		name: 'convertColors',
+		params: {
+			currentColor: true,
+		},
+	  },
 	],
-};
+  };
